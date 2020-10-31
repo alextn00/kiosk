e42ea754a5d127a9790765b607585cd49692eae0
alextn00 <alextn@naver.com>
2020-11-01 01:07:49 +0900

    kiosk project

---
+++ src/main/java/com/example/kiosk/option_size.java
@@ -0,0 +1,18 @@
+package com.example.kiosk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class option_size extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_size);
+    }
+
+    public void onClick(View view) {
+    }
+}
